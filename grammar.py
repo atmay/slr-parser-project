441c90dbb227b020c8480fc0e071b0d644d2ff01
atmay <liubov.burtseva@gmail.com>
2022-01-09 15:06:52 +0300

    grammar logic explained

--- grammar.py
+++ grammar.py
@@ -1,4 +1,7 @@
 class Grammar:
+    """ парсинг файла с правилами грамматики
+    """
+
     def __init__(self, grammar_str):
         self.grammar_str = '\n'.join(filter(None, grammar_str.splitlines()))
         self.grammar = {}
@@ -6,28 +9,32 @@ class Grammar:
         self.terminals = set()
         self.nonterminals = set()
 
+        # построчный разбор грамматики, каждая строка - продукция
         for production in list(filter(None, grammar_str.splitlines())):
             head, _, bodies = production.partition(' -> ')
 
+            # нетерминалом может быть только заглавная буква
             if not head.isupper():
                 raise ValueError(
-                    f'\'{head} -> {bodies}\': Head \'{head}\' is not capitalized to be treated as a nonterminal.')
+                    f'\'{head} -> {bodies}\': \'{head}\' - не заглавная буква и не может считаться нетерминалом.')
 
+            # первый нетерминал назначается стартовым
             if not self.start:
                 self.start = head
 
+            # дефолтное значение ключа - текущий нетерминал
             self.grammar.setdefault(head, set())
+            # текущий нетерминал добавляется во множество нетерминалов
             self.nonterminals.add(head)
-            bodies = {tuple(body.split()) for body in ' '.join(bodies.split()).split('|')}
+            # правая часть продукции разбивается на множество кортежей
+            bodies = {tuple(body.split()) for body in bodies.split('|')}
 
             for body in bodies:
-                if '^' in body and body != ('^',):
-                    raise ValueError(f'\'{head} -> {" ".join(body)}\': Null symbol \'^\' is not allowed here.')
-
                 self.grammar[head].add(body)
 
+                # составляем список терминалов и нетерминалов
                 for symbol in body:
-                    if not symbol.isupper() and symbol != '^':
+                    if not symbol.isupper():
                         self.terminals.add(symbol)
                     elif symbol.isupper():
                         self.nonterminals.add(symbol)
